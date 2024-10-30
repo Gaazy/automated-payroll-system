f85c1bd9b7c30c1e9f4f652a6db82709ae0a4676
Sourav <sourav.mondal0090@gmail.com>
2024-10-30 19:40:51 +0530

    Update payroll.sql

--- payroll_system/payroll.sql
+++ payroll_system/payroll.sql
@@ -6,7 +6,4 @@ select ((base_salary + ot_paid + other_pay)),
 (((base_salary + ot_paid + other_pay) - regular_gross_paid)*0.82) from salary;
 
 select * from payroll;
-#insert into payroll(tax) 
-#select ((base_salary + ot_paid + other_pay) - regular_gross_paid)*0.18 from salary;
-insert into payroll(salary_paid)
-select (((base_salary + ot_paid + other_pay) - regular_gross_paid)) from salary; 
+ 
